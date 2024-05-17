b79e3c6696f4e0b22080d54aeb1a4653067b02fb
QuentinGeerts <quentin.geerts@bstorm.be>
2024-05-17 15:19:21 +0200

    Initial commit

---
+++ Tools/Int32Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Tools;
+
+public static class Int32Extensions
+{
+    public static bool IsPrime(this int value)
+    {
+        if (value < 2) 
+            throw new InvalidOperationException( 
+                $"La valeur {value} ne peut pas être inférieure à 2 !");
+
+        int square = (int)Math.Sqrt(value);
+
+        for (int divider = 2; divider <= square; divider++)
+        {
+            if (value % divider == 0) return false;
+        }
+
+        return true;
+    }
+}
